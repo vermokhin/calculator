9f42019ca2001e69343e5487f23ae716592fcf33
Chocolateslayer <Chocolateslayer@yahoo.com>
2016-06-29 17:00:33 +0400

    Сложение

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Calculator
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = (int.Parse(textBox1.Text) + int.Parse(textBox2.Text)).ToString();
+        }
+    }
 }
